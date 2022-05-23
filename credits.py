57eafc7ceae1d4a6d54b91df88302dba1482b2a8
Thambir Nazrul Islam <Thambir.nazrul@outlook.fr>
2022-05-23 19:40:46 +0200

    e

--- credits.py
+++ credits.py
@@ -16,11 +16,10 @@ class exit_button:
             def __init__(self):
                 self.x = 10
                 self.y = 620
-                self.pressed = pygame.image.load('Images/Buttons/stop.pressed.png')
-                self.unpressed = pygame.image.load('Images/Buttons/stop.unpressed.png')
+                self.unpressed = pygame.image.load('Images\menu\quit_button.png')
 
                
-icon = pygame.image.load('Images/Buttons/stop.unpressed.png') 
+icon = pygame.image.load('Images\menu\quit_button.png') 
 pygame.display.set_icon(icon)  
 
 button_list = [exit_button]
@@ -40,7 +39,7 @@ def credits_menu(screen):
                 for event in pygame.event.get():
                     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                         if (i == exit_button):
-                            return
+                            main_menu_func()
                         
 
 
